e3f554dd5d6a6957e19803ba0f3302c1d1bc1c2c
codemon.ai <yong150@gmail.com>
2026-02-23 01:03:27 +0900

    Sentry 에러 모니터링 설정 (@sentry/nextjs)

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,26 @@
 import nextra from 'nextra'
+import { withSentryConfig } from '@sentry/nextjs'
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-export default withNextra({
+const nextConfig = withNextra({
   reactStrictMode: true,
+})
+
+export default withSentryConfig(nextConfig, {
+  // Sentry webpack plugin options
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: !process.env.CI,
+  // Source map upload disabled for now
+  sourcemaps: {
+    disable: true,
+  },
+  // Auto-instrumentation
+  autoInstrumentServerFunctions: true,
+  autoInstrumentMiddleware: true,
+  autoInstrumentAppDirectory: true,
 })
---
+++ pages/_error.tsx
@@ -0,0 +1,14 @@
+import * as Sentry from '@sentry/nextjs'
+import type { NextPageContext } from 'next'
+import NextErrorComponent from 'next/error'
+
+const CustomErrorComponent = (props: { statusCode: number }) => {
+  return <NextErrorComponent statusCode={props.statusCode} />
+}
+
+CustomErrorComponent.getInitialProps = async (contextData: NextPageContext) => {
+  await Sentry.captureUnderscoreErrorException(contextData)
+  return NextErrorComponent.getInitialProps(contextData)
+}
+
+export default CustomErrorComponent
