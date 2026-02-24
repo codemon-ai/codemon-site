aa8a7b2e2a838f23fcba9ba052473ddcf8197bc4
codemon.ai <yong150@gmail.com>
2026-02-25 00:02:48 +0900

    Google AdSense 스크립트 추가 (ca-pub-2049212498619879)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2049212498619879"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
       {GA_ID && (
         <>
           <Script
